58fddac170282190b4b4afbb3f368b5e448aefe6
OlegBergs <oleg.bergsma@gmail.com>
2026-06-11 14:29:22 +0200

    map tile change to Stadio Outdoors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // 1. Initialiseer de kaart gecentreerd op Utrecht
 const map = L.map('map').setView([52.0907, 5.1214], 14);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
+L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.{ext}', {
+    minZoom: 0,
+    maxZoom: 20,
+    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    ext: 'png'
 }).addTo(map);
-
 // 1b. Utrecht Bounding Box
 
 async function drawUtrechtMask() {
